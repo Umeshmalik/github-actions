f8bc5adae7625eb6754694f97b8b84357658b039
Umesh-malik-byjus <88026680+Umesh-malik-byjus@users.noreply.github.com>
2022-10-16 23:18:23 +0530

    added health check html file

--- routes.js
+++ routes.js
@@ -1,3 +1,5 @@
+const path = require('path');
+const express = require('express');
 const Router = require("express").Router();
 
 const pythonRoutes = require("./src/pythonmanagement/routes");
@@ -7,7 +9,7 @@ module.exports = () =>
     Router
     .get("/health", (req, res)=> {
         console.log("Server is running.")
-        res.status(200).send("Hey! I am up, start using me.")
+        res.status(200).sendFile(path.join(__dirname, 'static/index.html'))
     })
     .use("/python", pythonRoutes())
     .use("/node", nodeRoutes())
